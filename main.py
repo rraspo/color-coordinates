ab23677bca59ec304af40a1c5190b5d43b9967b3
Juan Porras <juanabrahamporras@gmail.com>
2022-07-26 16:51:30 -0500

    Add some trackbars code to manually test for a color range

--- main.py
+++ main.py
@@ -3,8 +3,22 @@
 import cv2
 import numpy as np
 
+
+def doNothing(x): pass
+
+
+cv2.namedWindow('trackbar')
+
 # Webcam no 0 is used to capture the frames
 cap = cv2.VideoCapture(0)
+
+cv2.createTrackbar('Hue Lower', 'trackbar', 0, 255, doNothing)
+cv2.createTrackbar('Saturation Lower', 'trackbar', 0, 255, doNothing)
+cv2.createTrackbar('Value Lower', 'trackbar', 0, 255, doNothing)
+
+cv2.createTrackbar('Hue Higher', 'trackbar', 255, 255, doNothing)
+cv2.createTrackbar('Saturation Higher', 'trackbar', 255, 255, doNothing)
+cv2.createTrackbar('Value Higher', 'trackbar', 255, 255, doNothing)
 
 # This drives the program into an infinite loop.
 while True:
@@ -12,21 +26,42 @@ while True:
     _, frame = cap.read()
     # Convert images from BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_blue = np.array([50, 50, 50])
-    upper_blue = np.array([255, 255, 255])
 
-    # Here we are defining range of blue color in HSV
-    # This creates a mask of blue coloured
-    # objects found in the frame.
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    # lower_blue = np.array([50, 50, 50])
+    # upper_blue = np.array([255, 255, 255])
+    lower_red = np.array([0, 0, 186])
+    upper_red = np.array([101, 168, 255])
+    # lower_green = np.array([121, 100, 100])
+    # upper_green = np.array([180, 100, 100])
+
+    # blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    red_mask = cv2.inRange(hsv, lower_red, upper_red)
+    # green_mask = cv2.inRange(hsv, lower_green, upper_green)
 
     # The bitwise and of the frame and mask is done so
     # that only the blue coloured objects are highlighted
     # and stored in res
-    res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    # mask = cv2.bitwise_or(blue_mask, green_mask, red_mask)
+    # res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    # l_h = cv2.getTrackbarPos('Hue Lower', 'trackbar')
+    # l_s = cv2.getTrackbarPos('Saturation Lower', 'trackbar')
+    # l_v = cv2.getTrackbarPos('Value Lower', 'trackbar')
+    #
+    # h_h = cv2.getTrackbarPos('Hue Higher', 'trackbar')
+    # h_s = cv2.getTrackbarPos('Saturation Higher', 'trackbar')
+    # h_v = cv2.getTrackbarPos('Value Higher', 'trackbar')
+    # l_bound = np.array([l_h, l_s, l_v])
+    # u_bound = np.array([h_h, h_s, h_v])
+    # mask = cv2.inRange(frame, l_bound, u_bound)
+
+    res = cv2.bitwise_and(frame, frame, mask=red_mask)
+
     cv2.imshow('frame', frame)
-    cv2.imshow('mask', mask)
+    cv2.imshow('mask', red_mask)
     cv2.imshow('res', res)
+    cv2.imshow('test', hsv)
 
     # 
 
